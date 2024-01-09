b65079cbfdf6227e3969f79ab1e482068480eb8a
Javier Arreguin Nila <111711706+JavierNila@users.noreply.github.com>
2024-01-09 15:56:15 -0600

    Add files via upload

---
+++ PHP/resultado.php
@@ -0,0 +1,20 @@
+<?php 
+include 'head2.html';
+
+//Recibiendo datos desde el Index.php usando el metodo POST
+$nombre = $_POST['nombre'];
+$edad = $_POST['edad'];
+$correo = $_POST['correo'];
+$contraseña = $_POST['contraseña'];
+
+//Mostrando los datos recibidos  (echo es para imprimir en php)
+echo "<p>Mi nombre es: $nombre</p>";
+echo "<p>Mi edad es: $edad</p>";
+echo "<p>Mi correo es: $correo</p>";
+echo "<p>Mi contraseña es: $contraseña</p>";
+echo "<a href='Index.php'> Back </a>"; //Boton para regresar
+
+
+
+
+?>
